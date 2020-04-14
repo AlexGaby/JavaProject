97175ff6c1f669f9668086d78e3922bd7353ca8d
ALEXANDRU NEMES <alexgabriel_n@yahoo.com>
2020-04-14 23:02:19 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Exercitiul 1
+        System.out.println("Hello world");
+        System.out.println("Numele meu este Alexandru");
+
+        //Exercitiul 2
+        int a,b;
+        a = 5;
+        b = 10;
+        int sum=a+b;
+        System.out.println();
+        System.out.println("Suma numerelor 5 si 10 este: "+sum);
+
+        //Exercitiul 3
+        double c,d;
+        c = 10;
+        d = 3;
+        double rez =c/d;
+        System.out.println();
+        System.out.println("Rezultatul impartirii numarului 10 la 3 este: " +rez);
+
+        //Exercitiul 4
+        System.out.println();
+        System.out.println(-5+8*6);
+        System.out.println((55+9) % 9);
+        System.out.println(20+-3*5/8);
+        System.out.println (5+15/3*2-8%3);
+
+    }
+}
